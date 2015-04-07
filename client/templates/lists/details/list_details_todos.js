552e1c34e74bc56b3cf27ff1fdc4c5a8d70051ba
jonas28 <jonas@steps.fm>
2015-04-07 18:10:42 +0200

    dragndrop reorder for todos in publisher dashboard

--- client/templates/lists/details/list_details_todos.js
+++ client/templates/lists/details/list_details_todos.js
@@ -1,5 +1,5 @@
 Template.listDetailsTodos.helpers({
     todos: function() {
-        return Todos.find({listId : this._id, status : 'published'});
+        return Todos.find({listId : this._id, status : 'published'}, {sort: {rank: 1}});
     }
 });
