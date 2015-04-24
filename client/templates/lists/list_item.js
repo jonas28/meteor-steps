2a97bbc8556b2efbe2f1d1046995764f7e49cbb4
jonas28 <jonas@steps.fm>
2015-04-24 14:21:40 +0200

    added exportsCount to list_item

--- client/templates/lists/list_item.js
+++ client/templates/lists/list_item.js
@@ -5,6 +5,9 @@ Template.listItem.helpers({
     todosCount: function() {
         return Todos.find({listId : this._id, status : 'published'}).count();
     },
+    exportsCount: function() {
+        return Exports.find({listId : this._id}).count();
+    },
     forksCount: function() {
         return Lists.find({original :  this._id}).count();
     },
