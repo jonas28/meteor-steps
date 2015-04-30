376bceeca2b9ef7fe9516806ab50dd8b6b9de0cb
jonas28 <jonas@steps.fm>
2015-04-30 19:15:59 +0200

    fixed issue #51, now its possible to create new todos from todo template in publisher

--- client/templates/publisher/publisher_todo_submit_2.js
+++ client/templates/publisher/publisher_todo_submit_2.js
@@ -1,10 +1,12 @@
-Template.publisherTodoSubmit.events({
+Template.publisherTodoSubmit2.events({
     'submit form': function(e) {
         e.preventDefault();
+
         var todo = {
             title: $(e.target).find('[name=title]').val(),
-            listId: this._id
+            listId: this.listId
         };
+
         Meteor.call('todoInsert', todo, function(error, result) {
             Router.go('publisherTodoPage', {listId : todo.listId, todoId: result._id });
         });
